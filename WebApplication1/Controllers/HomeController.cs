a43a4b7cc0fcd059b52eb32a7e12b7b5ef8dbde4
ugurkiymetli <34272634+ugurkiymetli@users.noreply.github.com>
2021-12-02 23:26:47 +0300

    WebApp project added. LoginFilter created.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using WebApplication1.Filters;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController( ILogger<HomeController> logger )
+        {
+
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [LoginFilter("admin")]
+        //[LoginFilter("superAdmin")]
+        //[LoginFilter("user")]
+
+        //if user is admin or superAdmin user can login, otherwise redirected to Error action
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Filters/LoginFilter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace WebApplication1.Filters
+{
+    public class LoginFilter : Attribute, IActionFilter
+    {
+        private readonly string _user;
+        public LoginFilter( string user )
+        {
+            _user = user;
+        }
+
+        public void OnActionExecuted( ActionExecutedContext context )
+        {
+            if ( _user == "admin" || _user == "superAdmin" )
+            {
+                Console.WriteLine($"{_user} logged in!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"{_user} not authorized!");
+                return;
+            }
+        }
+
+        public void OnActionExecuting( ActionExecutingContext context )
+        {
+            if ( _user == "admin" || _user == "superAdmin" )
+            {
+                return;
+            }
+            else
+            {
+                //Console.WriteLine($"{_user} not authorized!");
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { Controller = "Home", Action = "Error" }));
+                return;
+            }
+        }
+    }
+}
